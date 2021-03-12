9f4916e34de7d9b1c90736b9a3c5891f31e4a974
nupur <nupur.banerjee@iiitb.org>
2021-03-12 12:08:33 +0530

    tests updated

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,25 +11,51 @@ public class CalculatorTest {
 
     @Test
     public void squareRootPositive() {
-        assertEquals("Testing True Positive for squareRoot", 5.0, c.sqroot(25), delta);
-        assertEquals("Testing for True Negative for squareRoot", Double.NaN, c.sqroot(-25), delta);
+        assertEquals("Testing True Positive for squareRoot", 9.0, c.sqroot(81), delta);
+        assertEquals("Testing for True Negative for squareRoot", Double.NaN, c.sqroot(-36), delta);
     }
     @Test
     public void squareRootNegative() {
-        assertNotEquals("Testing False Negative for squareRoot", 6.0, c.sqroot(-24), delta);
-        assertNotEquals("Testing for False Positives for squareRoot", Double.NaN, c.sqroot(36), delta);
+        assertNotEquals("Testing False Negative for squareRoot", 6.0, c.sqroot(-36), delta);
+        assertNotEquals("Testing for False Positives for squareRoot", Double.NaN, c.sqroot(100), delta);
     }
 
     @Test
     public void factorialPositive() {
-        assertEquals("Testing True Positive for factorial", 24.0, c.factorial(4), delta);
-        assertEquals("Testing for True Negative for factorial", Double.NaN, c.factorial(-25), delta);
+        assertEquals("Testing True Positive for factorial", 120.0, c.factorial(5), delta);
+        assertEquals("Testing for True Negative for factorial", Double.NaN, c.factorial(-5), delta);
 
     }
 
     @Test
     public void factorialNegative() {
-        assertNotEquals("Testing False Negative for factorial", 6.0, c.factorial(-24), delta);
+        assertNotEquals("Testing False Negative for factorial", 6.0, c.factorial(-36), delta);
         assertNotEquals("Testing for False Positives for factorial", Double.NaN, c.factorial(36), delta);
+    }
+
+    @Test
+    public void nlogPositive() {
+        assertEquals("Testing True Positive for factorial", 0.0, c.nlog(1), delta);
+        assertEquals("Testing for True Negative for factorial", Double.NaN, c.nlog(-36), delta);
+
+    }
+
+    @Test
+    public void nlogNegative() {
+        assertNotEquals("Testing False Negative for factorial", 10.0, c.nlog(100), delta);
+        assertNotEquals("Testing for False Positives for factorial", Double.NaN, c.nlog(6), delta);
+    }
+
+    @Test
+    public void powerPositive() {
+        assertEquals("Testing True Positive for factorial", 25.0, c.power(5,2), delta);
+        assertEquals("Testing for True Negative for factorial", -25.0, c.power(-25,1), delta);
+
+    }
+
+    @Test
+    public void powerNegative() {
+        assertNotEquals("Testing False Negative for factorial", 6.0, c.power(-24,1), delta);
+        assertNotEquals("Testing for False Positives for factorial", Double.NaN, c.power(36,2), delta);
     }
 }
